6728a61db312b08c72d026b856bfda2aafa59d9b
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-08 21:33:56 -0700

    Updated to render html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Index Page'
+  #return 'Index Page'
+  return render_template('index_page.html')
 
 
 
