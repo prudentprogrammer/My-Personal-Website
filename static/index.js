0916196033c9cfde8865c9d6ecf52fd95b95d88d
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-16 00:37:26 -0700

    Cleaned up code

--- static/index.js
+++ static/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-  $('.parallax').parallax();
-
-  $('.dropdown-button').dropdown({
+  /* $('.dropdown-button').dropdown({
       inDuration: 300,
       outDuration: 225,
       constrain_width: true, // Does not change width of dropdown to that of the activator
@@ -9,13 +7,9 @@ $(document).ready(function(){
       gutter: 0, // Spacing from edge
       belowOrigin: true // Displays dropdown below the button
     }
-  );
-  // Initialize collapse button-collapse
- $('.button-collapse').sideNav({
-      menuWidth: 240, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
+  );*/
+
+  $('.parallax').parallax();
+  $('.sidenav').sidenav();
   $('.collapsible').collapsible();
 });
