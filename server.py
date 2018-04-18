075c933372bdd343a7224b4ca2694131f020cde4
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-18 00:32:26 -0700

    Finished the education page

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
 import datetime
+import csv
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -12,9 +14,24 @@ def index():
 def experiences():
     return render_template('experience.html')
 
+@app.route('/education')
+def education():
+    print(getClasses())
+    return render_template('education.html', classes_dict=getClasses())
+
 @app.context_processor
 def getCurrentDate():
   return dict(current_date = datetime.datetime.now().strftime("%d %B %Y"))
+
+# Helper function to extract classes taken at university
+def getClasses():
+    classes_dict = defaultdict(list)
+    file_handler = open('static/classes.csv', 'rb')
+    reader = csv.reader(file_handler)
+    for row in reader:
+        class_name, level = row[0].strip(), row[1].strip()
+        classes_dict[level].append(class_name)
+    return classes_dict
 
 if __name__ == '__main__':
   app.run(debug = True)
